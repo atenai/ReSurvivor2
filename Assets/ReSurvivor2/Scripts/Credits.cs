8dda52c237680e574a8b8235ad517157379f5401
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-10-19 19:44:54 +0900

    クレジットシーンを作成

---
+++ Assets/ReSurvivor2/Scripts/Credits.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// クレジット
+/// </summary> 
+public class Credits : OutGameManagerBase
+{
+    new void Start()
+    {
+        base.Start();
+    }
+
+    new void Update()
+    {
+        base.Update();
+    }
+}
