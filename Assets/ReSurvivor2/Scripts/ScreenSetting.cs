1bc40ac58f3c822080f1469c8bb6cc21e625d2b6
atenai <alfamod47@yahoo.co.jp>
2025-03-08 14:08:14 +0900

    マウスカーソルの処理を修正

--- Assets/ReSurvivor2/Scripts/ScreenSetting.cs
+++ Assets/ReSurvivor2/Scripts/ScreenSetting.cs
@@ -3,75 +3,49 @@ using UnityEditor;
 
 public class ScreenSetting : MonoBehaviour
 {
-    [Tooltip("マウスカーソルのオン/オフ")]
-    [SerializeField] bool isCursor = false;
+	[Tooltip("マウスカーソルのオン/オフ")]
+	[SerializeField] bool isCursor = false;
 
-    void Awake()
-    {
-#if UNITY_ANDROID//端末がAndroidだった場合の処理
-
-#endif //終了
-
+	void Awake()
+	{
 #if UNITY_EDITOR || UNITY_STANDALONE_WIN//Unityエディター上または端末がPCだった場合の処理
 
-        Screen.SetResolution(1920, 1080, true, 60);
+		Screen.SetResolution(1920, 1080, true, 60);
+		CursorActive();
 
 #endif //終了
+	}
 
-#if UNITY_STANDALONE_WIN//端末がPCだった場合の処理
-        CursorActive();
-#endif //終了
-
-    }
-
-    void Update()
-    {
+	void Update()
+	{
 #if UNITY_EDITOR//Unityエディター上での処理
-        //Tキーでマウスカーソルを出すorマウスカーソルを消す
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            isCursor = isCursor ? false : true;
-            CursorActive();
-        }
-#endif //終了   
 
-        // #if UNITY_EDITOR || UNITY_STANDALONE_WIN//Unityエディター上または端末がPCだった場合の処理
-        //         //Escapeキーでゲーム終了
-        //         if (Input.GetKeyDown(KeyCode.Escape))
-        //         {
-        //             Quit();//ゲーム終了
-        //         }
-        // #endif //終了   
-    }
+		//Tキーでマウスカーソルを出すorマウスカーソルを消す
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			isCursor = isCursor ? false : true;
+		}
+		CursorActive();
 
-    /// <summary>
-    /// マウスカーソルのオン/オフ処理 
-    /// </summary>
-    void CursorActive()
-    {
-        if (isCursor == false)
-        {
-            //マウスカーソルを消す
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else if (isCursor == true)
-        {
-            //マウスカーソルを出す
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
-    }
+#endif //終了    
+	}
 
-    /// <summary>
-    /// ゲーム終了
-    /// </summary> 
-    void Quit()
-    {
-#if UNITY_EDITOR
-        EditorApplication.isPlaying = false;
-#elif UNITY_STANDALONE
-        UnityEngine.Application.Quit();
-#endif
-    }
+	/// <summary>
+	/// マウスカーソルのオン/オフ処理 
+	/// </summary>
+	void CursorActive()
+	{
+		if (isCursor == false)
+		{
+			//マウスカーソルを消す
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+		else if (isCursor == true)
+		{
+			//マウスカーソルを出す
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
 }
