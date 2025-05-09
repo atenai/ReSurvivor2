734b1a24c6a2dd58c7ae31bd4130384482dae80a
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2025-05-09 23:14:05 +0900

    コンピューターに近づいたらカメラが切り替わるように作成

--- Assets/ReSurvivor2/Scripts/Computer.cs
+++ Assets/ReSurvivor2/Scripts/Computer.cs
@@ -5,55 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class Computer : MonoBehaviour
 {
-    [Tooltip("このコンピューターの名前")]
-    [SerializeField] InGameManager.ComputerTYPE thisComputerName;
+	[Tooltip("このコンピューターの名前")]
+	[SerializeField] InGameManager.ComputerTYPE thisComputerName;
 
-    void OnTriggerStay(Collider collider)
-    {
-        if (collider.CompareTag("Player"))
-        {
-            Debug.Log("<color=yellow>プレイヤー</color>");
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                Debug.Log("<color=green>Fキー</color>");
-                Save();
+	void OnTriggerStay(Collider collider)
+	{
+		if (collider.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("<color=yellow>プレイヤー</color>");
+			if (Input.GetKeyDown(KeyCode.F))
+			{
+				Debug.Log("<color=green>Fキー</color>");
+				Save();
 
-                if (InGameManager.SingletonInstance.IsMissionActive == false)
-                {
-                    //↓ここをミッションごとに変えるようにする必要がある
-                    //各ComputerTYPEに紐づいたミッションリストを取得して、そこから選択されたミッション内容を↓に反映すればいい
-                    var result = InGameManager.SingletonInstance.MissionSerch(thisComputerName);
-                    if (result != null)
-                    {
-                        Debug.Log("<color=red>ミッション開始</color>");
-                        InGameManager.SingletonInstance.IsMissionActive = true;
-                        InGameManager.SingletonInstance.TargetComputerName = result.TargetComputerName;
-                        Player.SingletonInstance.Minute = result.Minute;
-                        Player.SingletonInstance.Seconds = result.Seconds;
-                    }
-                }
-                else if (InGameManager.SingletonInstance.IsMissionActive == true)
-                {
-                    if (thisComputerName == InGameManager.SingletonInstance.TargetComputerName)
-                    {
-                        Debug.Log("<color=blue>ミッション終了</color>");
-                        InGameManager.SingletonInstance.IsMissionActive = false;
+				if (InGameManager.SingletonInstance.IsMissionActive == false)
+				{
+					//↓ここをミッションごとに変えるようにする必要がある
+					//各ComputerTYPEに紐づいたミッションリストを取得して、そこから選択されたミッション内容を↓に反映すればいい
+					var result = InGameManager.SingletonInstance.MissionSerch(thisComputerName);
+					if (result != null)
+					{
+						Debug.Log("<color=red>ミッション開始</color>");
+						InGameManager.SingletonInstance.IsMissionActive = true;
+						InGameManager.SingletonInstance.TargetComputerName = result.TargetComputerName;
+						Player.SingletonInstance.Minute = result.Minute;
+						Player.SingletonInstance.Seconds = result.Seconds;
+					}
+				}
+				else if (InGameManager.SingletonInstance.IsMissionActive == true)
+				{
+					if (thisComputerName == InGameManager.SingletonInstance.TargetComputerName)
+					{
+						Debug.Log("<color=blue>ミッション終了</color>");
+						InGameManager.SingletonInstance.IsMissionActive = false;
 
-                        //ゲームクリアー処理
-                        SceneManager.LoadScene("GameClear");
-                    }
-                }
-            }
-        }
-    }
+						//ゲームクリアー処理
+						SceneManager.LoadScene("GameClear");
+					}
+				}
+			}
+		}
+	}
 
-    /// <summary>
-    /// セーブ
-    /// </summary>
-    private void Save()
-    {
-        Debug.Log("<color=cyan>セーブ</color>");
-        PlayerPrefs.SetInt("KeyItem1", InGameManager.SingletonInstance.KeyItem1);
-        PlayerPrefs.Save();
-    }
+	/// <summary>
+	/// セーブ
+	/// </summary>
+	private void Save()
+	{
+		Debug.Log("<color=cyan>セーブ</color>");
+		PlayerPrefs.SetInt("KeyItem1", InGameManager.SingletonInstance.KeyItem1);
+		PlayerPrefs.Save();
+	}
 }
