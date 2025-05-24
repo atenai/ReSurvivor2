1fd7829d92d343be5c6912acde0b52f64d202f7a
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2025-05-24 22:26:01 +0900

    グレネードインジゲーターを作成

--- Assets/ReSurvivor2/Scripts/Enemy/Grenade.cs
+++ Assets/ReSurvivor2/Scripts/Enemy/Grenade.cs
@@ -4,68 +4,78 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    [UnityEngine.Tooltip("起爆時間")]
-    float detonationTime = 3.0f;
-    [UnityEngine.Tooltip("攻撃カウント")]
-    float count = 0.0f;
+	[UnityEngine.Tooltip("起爆時間")]
+	float detonationTime = 3.0f;
+	[UnityEngine.Tooltip("攻撃カウント")]
+	float count = 0.0f;
 
-    [Header("爆発")]
-    [Tooltip("爆発エフェクトのプレファブ")]
-    [SerializeField] GameObject mineExplosionEffectPrefab = null;
-    [Tooltip("爆発エフェクトの終了時間")]
-    [SerializeField] float mineExplosionEffectDestroyTime = 1.0f;
+	[Header("爆発")]
+	[Tooltip("爆発エフェクトのプレファブ")]
+	[SerializeField] GameObject mineExplosionEffectPrefab = null;
+	[Tooltip("爆発エフェクトの終了時間")]
+	[SerializeField] float mineExplosionEffectDestroyTime = 1.0f;
 
-    [Tooltip("地雷のSEプレファブ")]
-    [SerializeField] GameObject mineSePrefab = null;
-    [Tooltip("地雷のSE終了時間")]
-    [SerializeField] float mineSeEndtime = 1.0f;
+	[Tooltip("地雷のSEプレファブ")]
+	[SerializeField] GameObject mineSePrefab = null;
+	[Tooltip("地雷のSE終了時間")]
+	[SerializeField] float mineSeEndtime = 1.0f;
 
-    [Tooltip("爆発のあたり判定オブジェクトを生成")]
-    [SerializeField] GameObject mineExplosionColliderPrefab = null;
-    float mineExplosionColliderDestroyTime = 1.0f;
+	[Tooltip("爆発のあたり判定オブジェクトを生成")]
+	[SerializeField] GameObject mineExplosionColliderPrefab = null;
+	float mineExplosionColliderDestroyTime = 1.0f;
 
-    void Start()
-    {
+	void Start()
+	{
+		//グレネードマーカー作成
+		GrenadeIndicatorManager.SingletonInstance.InstanceIndicator(this);
+	}
 
-    }
+	void Update()
+	{
+		count = count + Time.deltaTime;
+		if (detonationTime < count)
+		{
+			count = 0.0f;
 
-    void Update()
-    {
-        count = count + Time.deltaTime;
-        if (detonationTime < count)
-        {
-            count = 0.0f;
+			Explosion();
+		}
+	}
 
-            Explosion();
-        }
-    }
+	/// <summary>
+	/// 爆発
+	/// </summary>
+	void Explosion()
+	{
+		if (mineSePrefab != null)
+		{
+			//SEオブジェクトを生成する
+			var se = Instantiate(mineSePrefab, this.gameObject.transform.position, Quaternion.identity);
+			Destroy(se, mineSeEndtime);
+		}
 
-    /// <summary>
-    /// 爆発
-    /// </summary>
-    void Explosion()
-    {
-        if (mineSePrefab != null)
-        {
-            //SEオブジェクトを生成する
-            var se = Instantiate(mineSePrefab, this.gameObject.transform.position, Quaternion.identity);
-            Destroy(se, mineSeEndtime);
-        }
+		if (mineExplosionEffectPrefab != null)
+		{
+			//爆発エフェクトオブジェクトを生成する	
+			var effect = Instantiate(mineExplosionEffectPrefab, this.gameObject.transform.position, Quaternion.identity);
+			Destroy(effect, mineExplosionEffectDestroyTime);
+		}
 
-        if (mineExplosionEffectPrefab != null)
-        {
-            //爆発エフェクトオブジェクトを生成する	
-            var effect = Instantiate(mineExplosionEffectPrefab, this.gameObject.transform.position, Quaternion.identity);
-            Destroy(effect, mineExplosionEffectDestroyTime);
-        }
+		if (mineExplosionColliderPrefab != null)
+		{
+			//爆発のあたり判定オブジェクトを生成する	
+			var mineExplosionCollider = Instantiate(mineExplosionColliderPrefab, this.gameObject.transform.position, Quaternion.identity);
+			Destroy(mineExplosionCollider, mineExplosionColliderDestroyTime);
+		}
 
-        if (mineExplosionColliderPrefab != null)
-        {
-            //爆発のあたり判定オブジェクトを生成する	
-            var mineExplosionCollider = Instantiate(mineExplosionColliderPrefab, this.gameObject.transform.position, Quaternion.identity);
-            Destroy(mineExplosionCollider, mineExplosionColliderDestroyTime);
-        }
+		Destroy(this.gameObject);
+	}
 
-        Destroy(this.gameObject);
-    }
+	/// <summary>
+	/// ゲームオブジェクトが非表示またはデストロイされた際に呼ばれる
+	/// </summary>
+	void OnDisable()
+	{
+		//敵マーカー削除
+		GrenadeIndicatorManager.SingletonInstance.DeleteIndicator(this);
+	}
 }
---
+++ Assets/ReSurvivor2/Scripts/Indicator/GrenadeIndicatorManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeIndicatorManager : MonoBehaviour
+{
+	//シングルトンで作成（ゲーム中に１つのみにする）
+	static GrenadeIndicatorManager singletonInstance = null;
+	public static GrenadeIndicatorManager SingletonInstance => singletonInstance;
+	[Tooltip("グレネード位置表示マーカー用プレハブ")]
+	[SerializeField] private GameObject grenadeIndicatorPrefab;
+	private Dictionary<Grenade, GameObject> indicatorList = new Dictionary<Grenade, GameObject>();   //マーカー一覧
+
+	void Awake()
+	{
+		//staticな変数instanceはメモリ領域は確保されていますが、初回では中身が入っていないので、中身を入れます。
+		if (singletonInstance == null)
+		{
+			singletonInstance = this;//thisというのは自分自身のインスタンスという意味になります。この場合、Playerのインスタンスという意味になります。
+			DontDestroyOnLoad(this.gameObject);//シーンを切り替えた時に破棄しない
+		}
+		else
+		{
+			Destroy(this.gameObject);//中身がすでに入っていた場合、自身のインスタンスがくっついているゲームオブジェクトを破棄します。
+		}
+	}
+
+	/// <summary>
+	/// グレネード表示マーカー生成
+	/// </summary>
+	public void InstanceIndicator(Grenade grenade)
+	{
+		//既に含まれていたら追加しない
+		if (!indicatorList.ContainsKey(grenade))
+		{
+			GameObject gameObject = (GameObject)Instantiate(grenadeIndicatorPrefab, this.transform.position, Quaternion.identity);
+			gameObject.transform.SetParent(this.transform);
+			indicatorList.Add(grenade, gameObject);
+			gameObject.GetComponent<Indicator>().Init(grenade.gameObject);
+		}
+		else
+		{
+			Debug.LogError("グレネード表示マーカーManager InstanceSensorMarker Failure.");
+		}
+	}
+
+	/// <summary>
+	/// グレネード表示マーカーの削除
+	/// </summary>
+	public void DeleteIndicator(Grenade grenade)
+	{
+		if (indicatorList.ContainsKey(grenade))
+		{
+			GameObject gameObject = indicatorList[grenade];
+			Destroy(gameObject);
+			indicatorList.Remove(grenade);
+		}
+	}
+}
