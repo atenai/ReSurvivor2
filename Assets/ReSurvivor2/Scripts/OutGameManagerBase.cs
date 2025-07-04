39fbdbbde677b461a7dbf1776bbafe5d2bce74a0
atenai <alfamod47@yahoo.co.jp>
2025-07-05 03:39:53 +0900

    シェーダーウォームアップを追加

--- Assets/ReSurvivor2/Scripts/OutGameManagerBase.cs
+++ Assets/ReSurvivor2/Scripts/OutGameManagerBase.cs
@@ -3,92 +3,129 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.Rendering;
 
 /// <summary>
 /// アウトゲームのマネージャー
 /// </summary>
 public class OutGameManagerBase : MonoBehaviour
 {
-    [SerializeField] protected string sceneName;
-    [SerializeField] protected Slider sliderLoading;
-    bool isLoadOnce = false;
+	[SerializeField] protected string sceneName;
+	[SerializeField] protected Slider sliderLoading;
+	bool isLoadOnce = false;
+	ShaderVariantCollection shaderVariantCollection;
 
-    protected void Start()
-    {
-        if (InGameManager.SingletonInstance != null)
-        {
-            Destroy(InGameManager.SingletonInstance.gameObject);
-        }
+	protected void Start()
+	{
+		if (InGameManager.SingletonInstance != null)
+		{
+			Destroy(InGameManager.SingletonInstance.gameObject);
+		}
 
-        if (Player.SingletonInstance != null)
-        {
-            Destroy(Player.SingletonInstance.gameObject);
-        }
+		if (Player.SingletonInstance != null)
+		{
+			Destroy(Player.SingletonInstance.gameObject);
+		}
 
-        if (PlayerCamera.SingletonInstance != null)
-        {
-            Destroy(PlayerCamera.SingletonInstance.gameObject);
-        }
+		if (PlayerCamera.SingletonInstance != null)
+		{
+			Destroy(PlayerCamera.SingletonInstance.gameObject);
+		}
 
-        if (UI.SingletonInstance != null)
-        {
-            Destroy(UI.SingletonInstance.gameObject);
-        }
-    }
+		if (UI.SingletonInstance != null)
+		{
+			Destroy(UI.SingletonInstance.gameObject);
+		}
 
-    protected void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Load(sceneName);
-        }
-    }
+		Shader.WarmupAllShaders();
 
-    /// <summary>
-    /// ロード
-    /// </summary> 
-    protected void Load(string nextSceneName)
-    {
-        if (isLoadOnce == false)
-        {
-            isLoadOnce = true;
-            StartCoroutine(LoadScene(nextSceneName));
-        }
-    }
+	}
 
-    /// <summary>
-    /// シーンをロードする
-    /// </summary>
-    IEnumerator LoadScene(string nextSceneName)
-    {
-        //スライダーの値を最低にする
-        sliderLoading.value = float.MinValue;
+	protected void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Load(sceneName);
+		}
+	}
 
-        //シーンをロード
-        AsyncOperation async = SceneManager.LoadSceneAsync(nextSceneName);
-        //シーンが勝手に切り替わらないようにする
-        async.allowSceneActivation = false;
-        //シーンをロードするまでのループ処理
-        while (async.isDone == false)
-        {
-            //ロード数値をスライダーに反映
-            sliderLoading.value = async.progress;
+	/// <summary>
+	/// ロード
+	/// </summary> 
+	protected void Load(string nextSceneName)
+	{
+		if (isLoadOnce == false)
+		{
+			isLoadOnce = true;
+			StartCoroutine(LoadScene(nextSceneName));
+		}
+	}
 
-            //ロード数値が0.9より同じかそれ以上大きくなったら中身を実行する
-            if (0.9f <= async.progress)
-            {
-                //スライダーの値を最大にする
-                sliderLoading.value = float.MaxValue;
+	/// <summary>
+	/// シーンをロードする
+	/// </summary>
+	IEnumerator LoadScene(string nextSceneName)
+	{
+		//スライダーの値を最低にする
+		sliderLoading.value = float.MinValue;
 
-                //フレームのラストまで待つ
-                yield return new WaitForEndOfFrame();
+		//シェーダーをロード
+		ShaderLoad();
 
-                //シーンを切り替える
-                async.allowSceneActivation = true;
-            }
+		//シーンをロード
+		AsyncOperation async = SceneManager.LoadSceneAsync(nextSceneName);
+		//シーンが勝手に切り替わらないようにする
+		async.allowSceneActivation = false;
+		//シーンをロードするまでのループ処理
+		while (async.isDone == false)
+		{
+			//ロード数値をスライダーに反映
+			sliderLoading.value = async.progress;
 
-            //1フレーム待つ
-            yield return null;
-        }
-    }
+			//ロード数値が0.9より同じかそれ以上大きくなったら中身を実行する
+			if (0.9f <= async.progress && 1.0f <= GetShaderWarmupProgressRate())
+			{
+				//スライダーの値を最大にする
+				sliderLoading.value = float.MaxValue;
+
+				//フレームのラストまで待つ
+				yield return new WaitForEndOfFrame();
+
+				//シーンを切り替える
+				async.allowSceneActivation = true;
+			}
+
+			//1フレーム待つ
+			yield return null;
+		}
+	}
+
+	/// <summary>
+	/// シェーダーをロード
+	/// </summary>
+	void ShaderLoad()
+	{
+		shaderVariantCollection = Resources.Load<ShaderVariantCollection>("ReSurvivor2ShaderVariants");
+
+		if (shaderVariantCollection != null)
+		{
+			shaderVariantCollection.WarmUp();
+			Debug.Log("シェーダーウォームアップ開始！");
+		}
+		else
+		{
+			Debug.LogWarning("Shader Variant Collection が見つかりません");
+		}
+	}
+
+	/// <summary>
+	/// シェーダーウォームアップの進捗を返す
+	/// </summary>
+	/// <returns>進捗(0～1)</returns>
+	float GetShaderWarmupProgressRate()
+	{
+		int variantCount = shaderVariantCollection.variantCount;            // variantの総数
+		int warmedUpCount = shaderVariantCollection.warmedUpVariantCount;   // Warmup済みのvariant数
+		return (float)warmedUpCount / (float)variantCount;
+	}
 }
