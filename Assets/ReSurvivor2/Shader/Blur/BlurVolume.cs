48a5ab783e81179631a2a92965373f41bc6538f5
kashiwabarayuta <alfamod47@yahoo.co.jp>
2026-01-04 17:22:42 +0900

    ブラーのシェーダーを作成

---
+++ Assets/ReSurvivor2/Shader/Blur/BlurVolume.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine.Rendering;
+
+[Serializable]
+public class BlurVolume : VolumeComponent
+{
+	// [HideInInspector] public BoolParameter IsActive = new(true, true);
+	private BoolParameter _active = new(true, true);
+	public bool IsActive => _active.overrideState;
+
+	public ClampedFloatParameter HorizontalBlur = new(0.05f, 0, 0.5f);
+	public ClampedFloatParameter VerticalBlur = new(0.05f, 0, 0.5f);
+}
